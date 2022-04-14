223dff569e093ad96f079f044e4683afb191cca5
Gongzai Li <ligongzainz@gmail.com>
2022-04-14 16:02:28 +1200

    done refactor

--- unils/face_swap.py
+++ unils/face_swap.py
@@ -3,7 +3,7 @@ import numpy as np
 from unils.delaunay_triangulation import calculate_delaunay_triangles
 from unils.warp_trianglation import affine_transformation
 import unils.helper as hf
-
+from unils.calculate_mask import get_calculate_mask
 
 
 def face_swap(img_ref, detector, predictor, face_landmark_number):
@@ -41,26 +41,7 @@ def face_swap(img_ref, detector, predictor, face_landmark_number):
 
     affine_transformation(hull1, hull2, delaunay_triangles, img_ref, img_warped)
 
-    # todo refactor here 2 =================================================================================================
-
-    # Calculate Mask
-    hull8U = []
-    for i in range(0, len(hull2)):
-        hull8U.append((hull2[i][0], hull2[i][1]))
-
-    mask = np.zeros(img_ref.shape, dtype=img_ref.dtype)  # https://numpy.org/doc/stable/reference/arrays.dtypes.html
-
-    cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
-
-    # todo what is that
-    r = cv2.boundingRect(np.float32([hull2]))
-
-    # todo tuple add tuple
-    center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
-
-    # Clone seamlessly.
-    output = cv2.seamlessClone(np.uint8(img_warped), img_ref, mask, center, cv2.NORMAL_CLONE)
-    # todo refactor here 2 =================================================================================================
+    output = get_calculate_mask(hull2, hull2, img_ref, img_warped)
 
     img_warped = np.copy(img_ref)
     delaunay_triangles = calculate_delaunay_triangles(rect, hull1)
@@ -68,24 +49,8 @@ def face_swap(img_ref, detector, predictor, face_landmark_number):
     if len(delaunay_triangles) == 0:
         return None
 
-    # todo refactor here 2 =================================================================================================
     affine_transformation(hull2, hull1, delaunay_triangles, img_ref, img_warped)
 
-    # Calculate Mask
-    hull8U = []
-    for i in range(0, len(hull2)):
-        hull8U.append((hull1[i][0], hull1[i][1]))
-
-    mask = np.zeros(img_ref.shape, dtype=img_ref.dtype)
-
-    cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
-
-    r = cv2.boundingRect(np.float32([hull1]))
-
-    center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
-
-    # Clone seamlessly.
-    output = cv2.seamlessClone(np.uint8(img_warped), output, mask, center, cv2.NORMAL_CLONE)
-    # todo refactor here 2 =================================================================================================
+    output = get_calculate_mask(hull1, hull2, output, img_warped)
 
     return output
